ed7c5aea4c050ef24e1e111b781f54e19b8ea484
ChillerDragon <chillerdragon@gmail.com>
2025-11-02 11:58:14 +0100

    Init

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+ruby '3.3.5'
+
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'rackup'
+gem 'puma'
---
+++ lib/main.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'json'
+
+class LogStreamer
+  def initialize(log_dir, logfile_name = "latest")
+    @log_dir = log_dir
+    @logfile_name = logfile_name
+
+    @logfile_name = find_latest_log(log_dir) if @logfile_name == "latest"
+
+    @logpath = File.join(@log_dir, @logfile_name)
+  end
+
+  def find_latest_log(_log_dir)
+    raise "not implemented"
+  end
+
+  def dump(offset, num)
+    lines = File.read(@logpath).split("\n")
+    max = lines.count
+    return { error: "offset too low" } if offset.negative?
+
+    return { error: "offset #{offset} is bigger than max #{max}" } if offset > max
+
+    num = max - offset if offset + num > max
+
+    {
+      offset: offset,
+      num: num,
+      max: lines.count,
+      lines: lines[offset..(offset + num)]
+    }
+  end
+end
+
+get '/log' do
+  streamer = LogStreamer.new("logs", "big.txt")
+  offset = params[:offset]
+  num = params[:num]
+  token = params[:token]
+
+  return { error: "missing arg offset" }.to_json if offset.nil?
+  return { error: "missing arg num" }.to_json if num.nil?
+  return { error: "missing arg token" }.to_json if token.nil?
+  return { error: "wrong token" }.to_json if token != "xxx"
+
+  offset = offset.to_i
+  num = num.to_i
+  streamer.dump(offset, num).to_json
+end
